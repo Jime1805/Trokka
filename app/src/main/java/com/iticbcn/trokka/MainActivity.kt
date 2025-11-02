da09c118fdbc1658134ba49ccf3566d0dbe54557
Jime1805 <18ejimen@gmail.com>
2025-11-02 23:48:12 +0100

    estructura programacio inici de sessio

--- app/src/main/java/com/iticbcn/trokka/MainActivity.kt
+++ app/src/main/java/com/iticbcn/trokka/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.iticbcn.trokka
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etCorreoInit: EditText
+    private lateinit var etPassInit: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
+        initUi()
+    }
+
+    private fun initComponents(){
+        etCorreoInit.findViewById(R.id.etCorreoInit)
+        etPassInit.findViewById(R.id.etPassInit)
     }
 }
