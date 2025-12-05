a57c4959bdcbd4b5748debc8185c0a873b3c750b
ldloler <marc.be.pi@gmail.com>
2025-12-05 19:04:03 +0100

    layout preferencies acabat

---
+++ app/src/main/java/com/iticbcn/trokka/PreferenciasAcrivity.kt
@@ -0,0 +1,14 @@
+package com.iticbcn.trokka
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PreferenciasAcrivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferencias_acrivity)
+    }
+}
