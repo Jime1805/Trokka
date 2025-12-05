5be47605df8b26508dff58f67fbaf9da3e202c4d
ldloler <marc.be.pi@gmail.com>
2025-12-05 18:23:40 +0100

    funcionalitat arreglada, visualitzar un objecte. No es podia entrar i petava la app.

--- app/src/main/java/com/iticbcn/trokka/ObjectActivity.kt
+++ app/src/main/java/com/iticbcn/trokka/ObjectActivity.kt
@@ -3,6 +3,7 @@ package com.iticbcn.trokka
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,17 +17,17 @@ class ObjectActivity : AppCompatActivity() {
     private lateinit var btn_mapa: ImageView
     private lateinit var btn_lupa: ImageView
     private lateinit var btn_profile: ImageView
-    private lateinit var btn_initChat: ImageView
+    private lateinit var btn_initChat: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_object)
 
         initComponents()
-        initListeners(usuario = "")
+        initListeners()
     }
 
-    private fun initListeners(usuario: String){
+    private fun initListeners(){
         imgFlechita.setOnClickListener {
             finish()
         }
@@ -34,10 +35,10 @@ class ObjectActivity : AppCompatActivity() {
             navigateToMaps()
         }
         btn_profile.setOnClickListener {
-            navigateToProfile(usuario)
+            navigateToProfile()
         }
         btn_lupa.setOnClickListener {
-            navigateToLoby(usuario)
+            navigateToLoby()
         }
         btn_initChat.setOnClickListener {
             navigateToChat()
@@ -48,14 +49,14 @@ class ObjectActivity : AppCompatActivity() {
         val intent = Intent(this, ChatActivity::class.java)
         startActivity(intent)
     }
-    private fun navigateToLoby(usuario: String){
+    private fun navigateToLoby(){
         val intent = Intent(this, Loby_Activity::class.java)
-        intent.putExtra(PERFIL, usuario)
+        /*intent.putExtra(PERFIL, usuario)*/
         startActivity(intent)
     }
-    private fun navigateToProfile(usuario: String){
+    private fun navigateToProfile(){
         val intent = Intent(this, PerfilActivity::class.java)
-        intent.putExtra(PERFIL, usuario)
+        /*intent.putExtra(PERFIL, usuario)*/
         startActivity(intent)
     }
     private fun navigateToMaps(){
