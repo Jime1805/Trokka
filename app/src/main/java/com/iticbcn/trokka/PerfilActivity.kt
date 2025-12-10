55a3bced122d823263467b5734cbc2452de8de93
Jime1805 <18ejimen@gmail.com>
2025-12-10 15:05:48 +0100

    menu desplegable acabat (falta la flechita del drawer)

--- app/src/main/java/com/iticbcn/trokka/PerfilActivity.kt
+++ app/src/main/java/com/iticbcn/trokka/PerfilActivity.kt
@@ -2,127 +2,143 @@ package com.iticbcn.trokka
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.cardview.widget.CardView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.iticbcn.trokka.Loby_Activity.Companion.PERFIL
-import com.iticbcn.trokka.MainActivity.Companion.LOBY
 
 class PerfilActivity : AppCompatActivity() {
+
     private lateinit var tvNombreUsuario: TextView
-    private lateinit var imgFlechita: ImageView
     private lateinit var btn_mapa: ImageView
     private lateinit var btn_lupa: ImageView
     private lateinit var btn_profile: ImageView
-    private lateinit var btn_preferencias: ImageView
 
-    private lateinit var cvEditarObjetos: CardView //Hecho
-    private lateinit var cvSubirObjetos: CardView //Hecho
-    private lateinit var cvFavs: CardView // Hecho
-    private lateinit var cvCanviarCompta: CardView // Hehco
+    private lateinit var cvEditarObjetos: CardView
+    private lateinit var cvSubirObjetos: CardView
+    private lateinit var cvFavs: CardView
+    private lateinit var cvCanviarCompta: CardView
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
-        val perfil: String =intent.extras?.getString(PERFIL)?: ""
+
+        val perfil: String = intent.extras?.getString(PERFIL) ?: ""
+
         initComponents()
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_flechita)
+
+        setupNavView()
         initListeners()
         initUi(perfil)
     }
 
-    private fun setNombreUsuario(perfil: String){
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_superior, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            R.id.iConfiguracio -> {
+                drawerLayout.openDrawer(GravityCompat.END)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setupNavView() {
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.iIdioma, R.id.iTema, R.id.iPrivacidad, R.id.iAmigos -> navigateToPreferencies()
+            }
+            drawerLayout.closeDrawer(GravityCompat.END)
+            true
+        }
+    }
+
+    private fun setNombreUsuario(perfil: String) {
         tvNombreUsuario.text = perfil
     }
-    private fun initUi(perfil: String){
+
+    private fun initUi(perfil: String) {
         setNombreUsuario(perfil)
     }
-    private fun navigateToLoby(){
-        val intent = Intent(this, Loby_Activity::class.java)
-        startActivity(intent)
+
+    private fun navigateToLoby() {
+        startActivity(Intent(this, Loby_Activity::class.java))
     }
 
-    private fun navigateToMapa(){
-        val intent = Intent(this, MapActivity::class.java)
-        startActivity(intent)
+    private fun navigateToMapa() {
+        startActivity(Intent(this, MapActivity::class.java))
     }
 
-    private fun navigateToProfile(){
-        val intent = Intent(this, PerfilActivity::class.java)
-        startActivity(intent)
+    private fun navigateToProfile() {
+        startActivity(Intent(this, PerfilActivity::class.java))
     }
 
-    private fun navigateToMain(){
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+    private fun navigateToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 
     private fun navigateToPreferencies() {
-        val intent = Intent(this, PreferenciasAcrivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, PreferenciasAcrivity::class.java))
     }
 
     private fun navigateToUploadObject() {
-        val intent = Intent(this, UploadObjectActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, UploadObjectActivity::class.java))
     }
 
     private fun navigateToFavs() {
-        val intent = Intent(this, FavoritosActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, FavoritosActivity::class.java))
     }
 
     private fun navigateToMyObjects() {
-        val intent = Intent(this, MyObjectsActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, MyObjectsActivity::class.java))
     }
 
     private fun initListeners() {
-        imgFlechita.setOnClickListener {
-            finish()
-        }
-        btn_mapa.setOnClickListener {
-            navigateToMapa()
-        }
-        btn_lupa.setOnClickListener {
-            navigateToLoby()
-        }
-        btn_profile.setOnClickListener {
-            navigateToProfile()
-        }
-        btn_preferencias.setOnClickListener {
-            navigateToPreferencies()
-        }
-        cvCanviarCompta.setOnClickListener {
-            navigateToMain()
-        }
-        cvSubirObjetos.setOnClickListener {
-            navigateToUploadObject()
-        }
-        cvFavs.setOnClickListener {
-            navigateToFavs()
-        }
-        cvEditarObjetos.setOnClickListener {
-            navigateToMyObjects()
-        }
+        btn_mapa.setOnClickListener { navigateToMapa() }
+        btn_lupa.setOnClickListener { navigateToLoby() }
+        btn_profile.setOnClickListener { navigateToProfile() }
 
+        cvCanviarCompta.setOnClickListener { navigateToMain() }
+        cvSubirObjetos.setOnClickListener { navigateToUploadObject() }
+        cvFavs.setOnClickListener { navigateToFavs() }
+        cvEditarObjetos.setOnClickListener { navigateToMyObjects() }
     }
 
-    private fun initComponents(){
-        imgFlechita = findViewById(R.id.imgFlechita)
+    private fun initComponents() {
         btn_mapa = findViewById(R.id.btn_mapa)
         btn_lupa = findViewById(R.id.btn_lupa)
         btn_profile = findViewById(R.id.btn_profile)
         tvNombreUsuario = findViewById(R.id.tvNombreUsuario)
-        btn_preferencias = findViewById(R.id.btn_preferencias)
 
         cvEditarObjetos = findViewById(R.id.cvEditarObjetos)
         cvSubirObjetos = findViewById(R.id.cvSubirObjetos)
         cvFavs = findViewById(R.id.cvFavs)
         cvCanviarCompta = findViewById(R.id.cvCanviarCompta)
+
+        toolbar = findViewById(R.id.cvHeader)
+        navView = findViewById(R.id.navView)
+        drawerLayout = findViewById(R.id.main)
     }
 }
