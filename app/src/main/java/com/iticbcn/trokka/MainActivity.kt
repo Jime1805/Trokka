1df90068558b1b5614bcf3931f89180e2eb0e6b0
ldloler <marc.be.pi@gmail.com>
2025-12-12 19:47:23 +0100

    Splash Screen afegida

--- app/src/main/java/com/iticbcn/trokka/MainActivity.kt
+++ app/src/main/java/com/iticbcn/trokka/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -23,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initComponents()
