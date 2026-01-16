838c9a8564ea929879ee276ec881486e0b60e61b
ldloler <marc.be.pi@gmail.com>
2026-01-16 19:27:59 +0100

    Afegi RecyclerView amb filtrat a la pantalla de Loby.

--- app/src/main/java/com/iticbcn/trokka/Loby_Activity.kt
+++ app/src/main/java/com/iticbcn/trokka/Loby_Activity.kt
@@ -3,38 +3,34 @@ package com.iticbcn.trokka
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.iticbcn.trokka.MainActivity.Companion.LOBY
 
 class Loby_Activity : AppCompatActivity() {
 
-    private lateinit var tvUser: TextView
-    private lateinit var iv_ImagenObjeto: ImageView
     private lateinit var bottomNav: BottomNavigationView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MyAdapterLoby
+    private lateinit var svSerch: SearchView
 
-    companion object{
+    companion object {
         const val PERFIL = "perfil"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loby)
-        val usuario: String =intent.extras?.getString(LOBY)?: ""
+        val usuario: String = intent.extras?.getString(LOBY) ?: ""
         initComponents()
         initListeners(usuario)
-        initUi(usuario)
     }
 
-    private fun initListeners(usuario: String){
-        iv_ImagenObjeto.setOnClickListener {
-            navigateToObject()
-        }
+    private fun initListeners(usuario: String) {
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.mapa_fragment -> navigateToMaps()
@@ -42,35 +38,84 @@ class Loby_Activity : AppCompatActivity() {
             }
             true
         }
+        itemSearchView()
     }
 
-    private fun  navigateToObject() {
+    private fun itemSearchView() {
+        svSerch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                adapter.filtrar(query ?: "")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filtrar(newText ?: "")
+                return true
+            }
+        })
+    }
+
+    /*private fun navigateToObject() {
         val intent = Intent(this, ObjectActivity::class.java)
         startActivity(intent)
-    }
+    }*/
 
-    private fun navigateToProfile(usuario: String){
+    private fun navigateToProfile(usuario: String) {
         val intent = Intent(this, PerfilActivity::class.java)
         intent.putExtra(PERFIL, usuario)
         startActivity(intent)
     }
-    private fun navigateToMaps(){
+
+    private fun navigateToMaps() {
         val intent = Intent(this, MapActivity::class.java)
         startActivity(intent)
     }
 
-    private fun initUi(usuario: String) {
-        setCorreo(usuario)
-    }
-
-    private fun setCorreo(usuario: String){
-        tvUser.text = "Benvenido: $usuario"
-    }
-
-    private fun initComponents(){
-        tvUser = findViewById(R.id.tvUser)
-        iv_ImagenObjeto = findViewById(R.id.ivImagenObjeto)
+    private fun initComponents() {
+        initRecycler()
         bottomNav = findViewById(R.id.bottom_navigation)
         bottomNav.setBackgroundColor(Color.TRANSPARENT)
+        svSerch = findViewById(R.id.sv_serch)
+    }
+
+    private fun initRecycler() {
+        // 1. Obtenir referència al RecyclerView del layout
+        recyclerView = findViewById(R.id.rv_loby)
+
+        // 2. Configurar LayoutManager (com es col·loquen les files)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // 3. Crear llista de dades (des de DataSource o directament)
+        val items = DataSource.items
+
+        // 4. Crear l'Adapter passant les dades + funció de callback per clics
+        adapter = MyAdapterLoby(
+            items = items,
+            onItemClick = { item ->
+                // AQUÍ gestionem el clic: mostrem un Toast amb el títol
+
+                if (item.isFav) {
+                    item.isFav = false
+                    Toast.makeText(
+                        this,
+                        "Has eliminat de preferits: " + item.titol,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    initRecycler()
+                } else {
+                    item.isFav = true
+                    Toast.makeText(
+                        this,
+                        "Has afegit a preferits: " + item.titol,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    initRecycler()
+                }
+
+            }
+        )
+
+        // 5. Assignar l'Adapter al RecyclerView
+        recyclerView.adapter = adapter
     }
 }
