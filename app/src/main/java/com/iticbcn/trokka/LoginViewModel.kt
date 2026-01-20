9f89b1bafe37427ac179421759bbd51f278d0c20
ldloler <marc.be.pi@gmail.com>
2026-01-20 19:55:07 +0100

    Pasando Login a ViewModel, cosas con TODO.

---
+++ app/src/main/java/com/iticbcn/trokka/LoginViewModel.kt
@@ -0,0 +1,43 @@
+package com.iticbcn.trokka
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.io.path.Path
+
+class LoginViewModel : ViewModel() {
+
+    private val _state = MutableLiveData<Pair<Boolean, String>>(Pair(false, ""))
+    val state: LiveData<Pair<Boolean, String>> = _state
+
+    fun login(username: String, pass: String) {
+        if (username == UserLogin.username) {
+            _state.value = proofPassword(pass)
+        }
+    }
+
+    /*private fun proofEmail(email: String): Boolean {
+        val emailPattern = android.util.Patterns.EMAIL_ADDRESS
+        if (!emailPattern.matcher(email).matches()) {
+            etCorreoReg.error = "Correo electrónico no valido"
+            return false
+        }
+        return true
+    }*/
+
+    private fun proofPassword(password: String): Pair<Boolean, String> {
+        if (password.length !in 6..16) {
+            return Pair(false, "La contraseña debe tener entre 6 y 16 caracteres")
+        }
+
+        val passwordPattern = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$")
+        if (!password.matches(passwordPattern)) {
+            return Pair(
+                false,
+                "La contraseña debe de tener al menos una mayúscula, una minúscula y un número"
+            )
+        }
+
+        return Pair(true, "Inici de sessió correcte.")
+    }
+}
---
+++ app/src/main/java/com/iticbcn/trokka/UserLogin.kt
@@ -0,0 +1,6 @@
+package com.iticbcn.trokka
+
+object UserLogin {
+    val username = "user"
+    val pass = "User123"
+}
