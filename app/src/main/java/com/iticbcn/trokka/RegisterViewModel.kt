f9adf3980c8a559f46937ff573f8c54f2b076b1e
Jime1805 <18ejimen@gmail.com>
2026-02-26 15:00:14 +0100

    mirar register para que funcione los endpoints

--- app/src/main/java/com/iticbcn/trokka/RegisterViewModel.kt
+++ app/src/main/java/com/iticbcn/trokka/RegisterViewModel.kt
@@ -1,37 +1,72 @@
 package com.iticbcn.trokka
 
+import android.util.Log
 import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class RegisterViewModel: ViewModel() {
+
     private val _state = MutableLiveData<Pair<Boolean, String>>()
     val state: LiveData<Pair<Boolean, String>> = _state
 
-    fun register(
-        username: String,
-        email: String,
-        password: String,
-        rePassword: String
-    ) {
+    fun register(username: String, email: String, password: String, rePassword: String):Boolean {
+
         if (username.isBlank() || email.isBlank() || password.isBlank() || rePassword.isBlank()) {
             _state.value = Pair(false, "Todos los campos son obligatorios")
-            return
+            return false
         }
 
         if (!proofEmail(email)) {
             _state.value = Pair(false, "Correo electrónico no válido")
-            return
+            return false
         }
 
         val passwordResult = proofPassword(password, rePassword)
         if (!passwordResult.first) {
             _state.value = passwordResult
-            return
+            return false
         }
+        val funciona = createUser(username, email, password)
+        if (!funciona){
+            return false
+        }
+        Log.d("REGISTER", "Entra aqui register")
+        return true
+    }
 
-        _state.value = Pair(true, "Registro correcto")
+    private fun createUser(username: String, email: String, password: String): Boolean {
+        var funciona: Boolean = false
+
+        viewModelScope.launch {
+
+            try {
+                Log.d("REGISTER", "Entra aqui")
+
+                val response = ClientAPI.UsuariAPI().postUsers(
+                    UsuariRequest(username, email, password)
+                )
+
+                Log.d("REGISTER", "Code = ${response.code()}")
+                Log.d("REGISTER", "Body = ${response.body()}")
+                Log.e("REGISTER", "ErrorBody = ${response.errorBody()?.string()}")
+
+                if (response.isSuccessful) {
+                    funciona = true;
+                    _state.value = Pair(true, response.body() ?: "Registro correcto")
+                } else {
+                    _state.value = Pair(false, response.errorBody()?.string() ?: "Error servidor")
+                }
+
+            } catch (e: Exception) {
+                funciona = false
+                _state.value = Pair(false, "Error de conexión")
+            }
+        }
+        return funciona
     }
 
     private fun proofEmail(email: String): Boolean {
@@ -39,22 +74,20 @@ class RegisterViewModel: ViewModel() {
     }
 
     private fun proofPassword(password: String, rePassword: String): Pair<Boolean, String> {
+
         if (password.length !in 6..16) {
             return Pair(false, "La contraseña debe tener entre 6 y 16 caracteres")
         }
 
         val passwordPattern = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$")
         if (!password.matches(passwordPattern)) {
-            return Pair(
-                false,
-                "La contraseña debe tener al menos una mayúscula, una minúscula y un número"
-            )
+            return Pair(false, "Debe tener mayúscula, minúscula y número")
         }
 
         if (password != rePassword) {
             return Pair(false, "Las contraseñas no coinciden")
         }
 
-        return Pair(true, "Contraseña válida")
+        return Pair(true, "OK")
     }
 }
