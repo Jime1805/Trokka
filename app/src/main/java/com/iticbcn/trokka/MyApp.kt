da60b2d7da76033dca3e0022d07639251cd31ab5
Jime1805 <18ejimen@gmail.com>
2026-04-10 19:04:21 +0200

    VoiceChat i comandos por voz

--- app/src/main/java/com/iticbcn/trokka/MyApp.kt
+++ app/src/main/java/com/iticbcn/trokka/MyApp.kt
@@ -1,6 +1,7 @@
 package com.iticbcn.trokka
 
 import android.app.Application
+import android.content.Intent
 import com.google.firebase.FirebaseApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,6 +17,12 @@ class MyApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        VoiceChat.initObjects(this)
+
+        VoiceChat.initiListeners{command ->
+            handleVoiceCommand(command)
+        }
+
         FirebaseApp.initializeApp(this)
         inicio = Timestamp(System.currentTimeMillis())
 
@@ -33,5 +40,26 @@ class MyApp: Application() {
         inicio = final
 
         return tiempoTotal
+    }
+
+    private fun handleVoiceCommand(command: String?) {
+        when {
+            command?.contains("abrir mapa") == true -> {
+                val intent = Intent(this, MapActivity::class.java)
+                startActivity(intent)
+            }
+            command?.contains("abrir inicio") == true -> {
+                val intent = Intent(this, Loby_Activity::class.java)
+                startActivity(intent)
+            }
+            command?.contains("abrir perfil") == true -> {
+                val intent = Intent(this, PerfilActivity::class.java)
+                startActivity(intent)
+            }
+            command?.contains("cambiar cuenta") == true -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/iticbcn/trokka/VoiceChat.kt
@@ -0,0 +1,49 @@
+package com.iticbcn.trokka
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+
+object VoiceChat {
+    private var recognizer: SpeechRecognizer ?= null
+    private lateinit var recognizerIntent: Intent
+    private var callback: ((String) -> Unit) ?= null
+
+    fun initObjects(context: Context){
+        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ca-ES") // o "es-ES"
+        }
+        if (recognizer == null){
+            recognizer = SpeechRecognizer.createSpeechRecognizer(context)
+
+        }
+    }
+
+    fun initiListeners(onResult: (String?) -> Unit){
+        this.callback =  onResult
+        recognizer?.setRecognitionListener(object : RecognitionListener {
+            override fun onResults(results: Bundle?) {
+                val spokenText: String = results
+                    ?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                    ?.get(0)
+                    ?.lowercase().toString()
+                callback?.invoke(spokenText)
+            }
+
+            override fun onError(error: Int) {}
+            override fun onReadyForSpeech(params: Bundle?) {}
+            override fun onBeginningOfSpeech() {}
+            override fun onRmsChanged(rmsdB: Float) {}
+            override fun onBufferReceived(buffer: ByteArray?) {}
+            override fun onEndOfSpeech() {}
+            override fun onPartialResults(partialResults: Bundle?) {}
+            override fun onEvent(eventType: Int, params: Bundle?) {}
+        })
+
+        recognizer?.startListening(recognizerIntent)
+    }
+}
